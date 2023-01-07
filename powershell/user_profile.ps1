a0de7b3f26762ca2f39d2a3213a677b9accb0221
Andy <andytruong1997@hotmail.com>
2023-01-07 15:28:11 +1100

    feat: 🎸 added powershell config

---
+++ powershell/user_profile.ps1
@@ -0,0 +1,20 @@
+[console]::InputEncoding = [console]::OutputEncoding = New-Object System.Text.UTF8Encoding
+
+Import-Module posh-git
+oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH\powerline.omp.json" | Invoke-Expression
+
+$PSStyle.FileInfo.Directory = ""
+
+Import-Module -Name Terminal-Icons
+
+Set-PSReadLineOption -EditMode Emacs
+Set-PSReadLineOption -BellStyle None
+Set-PSReadLineKeyHandler -Chord 'Ctrl+d' -Function DeleteChar
+Set-PSReadLineOption -PredictionSource History
+
+Import-Module PSFzf
+Set-PsFzfOption -PSReadlineChordProvider 'Ctrl+f' -PSReadlineChordReverseHistory 'Ctrl+r'
+
+# Alias
+Set-Alias vim nvim
+Set-Alias grep findstr
