03cc22b115c43b6291ccd6b28ef72efc830a5d1f
Fengboi <andytruong1997@hotmail.com>
2023-01-21 03:00:29 +1100

    feat: adjusted nvimtree bs

--- nvim/lua/fengboi/plugins/nvim-tree.lua
+++ nvim/lua/fengboi/plugins/nvim-tree.lua
@@ -4,4 +4,31 @@ if not setup then return end
 vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
 
-nvimtree.setup()
+vim.cmd([[ highlight NvimTreeIndentMarker guifg=#3FC5FF ]])
+
+nvimtree.setup({
+  renderer = {
+    icons = {
+      glyphs = {
+        folder = {
+          arrow_closed = "",
+          arrow_open = "",
+        },
+      },
+    },
+  },
+  actions = {
+    open_file = {
+      window_picker = {
+        enable = false,
+      },
+    },
+  },
+  tab = {
+    sync = {
+      open = true,
+      close = true,
+      ignore = {},
+    },
+  },
+})
